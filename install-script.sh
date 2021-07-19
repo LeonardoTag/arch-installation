fe2528d4e06dbb0b4c40692a857410073bfc3ded
LeonardoTag <leoftag@protonmail.com>
2021-07-19 02:51:32 +0000

    Improving wifi connection system.

--- install-script.sh
+++ install-script.sh
@@ -5,15 +5,16 @@ loadkeys br-abnt2
 
 # Setting Internet Connection
 
-while [true]
+while [ 1 ]
 do
-	ping -c 1 archlinux.org > /dev/null && echo "Internet connection found." && break
-	read -p "Internet connection not found. Try again [1] or setup Wifi [2]?" DECISION
+	ping -c 1 archlinux.org > /dev/null 2> /dev/null && echo "Internet connection found." && break
+	echo "Internet connection not found."
+	read -p "Try again [1] or setup Wifi [2]? " DECISION
 	[ $DECISION = "1" ] && continue
 	if [ $DECISION = "2" ]; then
-		STATIONS=$("iwctl device list | grep ' on ' | grep -o -P '^ +\S+' | tr -d ' ' done")
-		[ wc -l $STATIONS = "0" ] && echo "No wifi device found." && continue
-		if [ wc -l $STATIONS = "1" ]; then
+		STATIONS=$(iwctl device list | grep ' on ' | grep -o -P '^ +\S+' | tr -d ' ')
+		[ "$(echo $STATIONS | wc -l)" = "0" ] && echo "No wifi device found." && continue
+		if [ "$(echo $STATIONS | wc -l)" = "1" ]; then
 			echo "Wifi device $STATIONS found."
 			STATION=$STATIONS
 		else
@@ -21,12 +22,18 @@ do
 			echo $STATIONS
 			read -p "Device to be used: " STATION
 		fi
-		iwctl station $STATION scan || echo "Invalid device." && continue
+		iwctl station $STATION scan || ( echo "Invalid device." && continue )
 		iwctl station $STATION get-networks
-		NETWORKS=$("iwctl station $STATION get-networks | grep '*' | grep -o -P '^ +\S+' | tr -d ' ' done")
-		read -p "Network to be connected to: "
+		#NETWORKS="$(iwctl station $STATION get-networks | grep '*')"
+		#NETWORKS=${NETWORKS:4}
+		#echo $NETWORKS
+		read -p "Network to be connected to: " NETWORK
+		read -s -p "Passphrase: " PASSPHRASE
+		iwctl --passphrase=$PASSPHRASE station $STATION connect "$NETWORK" || ( echo "Connection failed. Please try again." && continue )
+		sleep 1
+		continue
 	else
 		echo "Option unknown."
 		continue
 	fi
-		
+done
