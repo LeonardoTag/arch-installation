60199d665bb88deaaf16134b0246164aa0c761ba
Leonardo Tag <leoftag@protonmail.com>
2021-07-19 21:54:22 -0400

    Adding options for keyboard layout, boot mode, system clock and disk preparation.

--- install-script.sh
+++ install-script.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
 # Setting Keyboard Layout
-loadkeys br-abnt2
-
-# Setting Internet Connection
+echo
+echo "> Setting Keyboard Layout"
 
 while [ 1 ]
 do
-	ping -c 1 archlinux.org > /dev/null 2> /dev/null && echo "Internet connection found." && break
+	read -p "Keyboard layout: [Default: br-abnt2] " LAYOUT
+	[ $LAYOUT = "" ] && LAYOUT="br-abnt2"
+	loadkeys $LAYOUT &> /dev/null || ( echo "Invalid layout. Please try again." && continue )
+
+# Identifying Boot Mode
+echo
+echo "> Identifying Boot Mode"
+
+ls /sys/firmware/efi/efivars &> /dev/null && UEFI=1 || UEFI=0
+[ $UEFI = "1" ] && echo "-> UEFI" || echo "-> Legacy BIOS"
+
+# Setting Internet Connection
+echo
+echo "> Setting Internet Connection"
+
+while [ 1 ]
+do
+	ping -c 1 archlinux.org &> /dev/null && echo "Internet connection found." && break
 	echo "Internet connection not found."
-	read -p "Try again [1] or setup Wifi [2]? " DECISION
+	echo "[1] Try again"
+	echo "[2] Setup wireless connection"
+	read -p ": " DECISION
 	[ $DECISION = "1" ] && continue
 	if [ $DECISION = "2" ]; then
 		STATIONS=$(iwctl device list | grep ' on ' | grep -o -P '^ +\S+' | tr -d ' ')
 		[ "$(echo $STATIONS | wc -l)" = "0" ] && echo "No wifi device found." && continue
 		if [ "$(echo $STATIONS | wc -l)" = "1" ]; then
-			echo "Wifi device $STATIONS found."
+			echo "Wireless device found."
 			STATION=$STATIONS
 		else
 			echo "Multiple wifi devices found. Please choose one."
@@ -33,7 +51,43 @@ do
 		sleep 1
 		continue
 	else
-		echo "Option unknown."
+		echo "Invalid option."
 		continue
 	fi
 done
+
+# Updating System Clock
+echo
+echo "> Updating System Clock"
+timedatectl set-ntp true
+
+# Preparing For LUKS Encryption
+modprobe dm-crypt
+modprobe dm-mod
+
+# Preparing Disks
+echo
+echo "> Preparing Disks"
+echo "$ lsblk"
+lsblk
+
+while [ 1 ]
+do
+	read -p "Disk to partition :" DISK
+	read -p "Are you sure you want to overwrite whatever is in $DISK? [y/N]" DECISION
+	( [ ${DECISION^^} = "Y" ] || [ ${DECISION^^} = "YES" ] ) || continue
+done
+
+echo
+echo "INSTRUCTIONS:"
+echo
+if [ $UEFI = "0" ]; then
+	echo "Boot mode: Legacy BIOS"
+	echo "Label type: DOS"
+else
+	echo "Boot mode: UEFI"
+	echo "Label type: GPT"
+
+
+cfdisk /dev/sda
+
